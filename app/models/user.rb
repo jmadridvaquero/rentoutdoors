d02c67e361c07a1301ca71f227b5576cc5c75abb
Javier Madrid <jmadridvaquero@gmail.com>
2019-05-21 11:08:57 -0400

    added photo uploader and carrierwave gems, user signup form done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  mount_uploader :avatar_photo, PhotoUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
