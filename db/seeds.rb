a93420ad5bd57f59bfade59cdb79da4dc500c951
Diego Ulibarri <diegoulhe@outlook.com>
2019-05-21 15:54:43 -0400

    Modified the seeds, create shared cards and render in the index, add css animation to the cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,3 @@
 
 #Destroy all the users and equipment
 Equipment.destroy_all
-
-#Creating 5 equipment
-Equipment.create!(name: "Skateboard", description: "Nice Skateboard", sport: "Skateboarding", photo: "skateboard.png", user_id: 1)
-Equipment.create!(name: "Surfboard", description: "Nice Surfboard", sport: "Surfing", photo: "Surfboard.png", user_id: 1)
