4eb8b9faab310c4496867427d66243afdfdf448c
Simon dHarcourt <simondharcourt@gmail.com>
2019-05-21 17:06:22 -0400

    shit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_171416) do
+ActiveRecord::Schema.define(version: 2019_05_20_173345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 2019_05_21_171416) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "price"
     t.index ["user_id"], name: "index_equipment_on_user_id"
   end
 
@@ -45,9 +44,6 @@ ActiveRecord::Schema.define(version: 2019_05_21_171416) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
-    t.string "zipcode"
-    t.string "avatar_photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
