bc617b0da5b352872f201a043c777fcea962196a
Simon dHarcourt <simondharcourt@gmail.com>
2019-05-23 11:25:34 -0400

    correction map

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,12 +1,17 @@
 // app/javascript/components/banner.js
 import Typed from 'typed.js';
 
+const typedElement = document.getElementById('banner-typed-text');
+
+
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["Welcome to RentOutdoors", "Come to us"],
-    typeSpeed: 35,
-    loop: true,
-  });
+  if (typedElement) {
+    new Typed('#banner-typed-text', {
+      strings: ["Welcome to RentOutdoors", "Come to us"],
+      typeSpeed: 35,
+      loop: true,
+    });
+  }
 }
 
 export { loadDynamicBannerText };
