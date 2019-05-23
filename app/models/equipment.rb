92152e785847b6a3a929b0a479e531fda3a757cf
Diego Ulibarri <diegoulhe@outlook.com>
2019-05-23 10:23:03 -0400

    Add the address in the Edit button, correct maps

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -6,48 +6,11 @@ class Equipment < ApplicationRecord
   validates :sport, presence: true
   validates :photo, presence: true
   validates :price, presence: true
-  validates :description, presence: true
+  validates :address, presence: true
   # validates :address, presence: true
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-  SPORTS = ["Category","Basketball", "Soccer", "Cricket", "Ballet", "Kayaking", "Rockclimbing",
-  "Fishing", "Archery",
-  "Badminton",
-  "Cricket",
-  "Bowling",
-  "Boxing",
-  "Curling",
-  "Tennis",
-  "Skateboarding",
-  "Surfing",
-  "Hockey",
-  "Figure skating",
-  "Yoga",
-  "Fencing",
-  "Fitness",
-  "Gymnastics",
-  "Karate",
-  "Volleyball",
-  "Weightlifting",
-  "Basketball",
-  "Baseball",
-  "Rugby",
-  "Wrestling",
-  "High jumping",
-  "Hang gliding",
-  "Car racing",
-  "Cycling",
-  "Running",
-  "Table tennis",
-  "Fishing",
-  "Judo",
-  "Climbing",
-  "Pool",
-  "Shooting",
-  "Horse racing",
-  "Horseback riding",
-  "Golf",
-  "Canoeing"]
+  SPORTS = ["Beach", "Mountain", "River"]
 end
