4d1d31766971bdd893ec506aaf7ef7b2f742edba
Robert Simon <robertsimonmedia@gmail.com>
2019-05-23 12:40:56 -0400

    added popup to confirm and redirect to bookings page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,6 +9,7 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
+
     authorize @booking
   end
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,23 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 import { initMapbox } from '../plugins/init_mapbox';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 initMapbox();
+import { initSweetalert } from '../plugins/init_sweetalert';
+
+initSweetalert('#booking-confirmation', {
+  title: "Your booking has been sent to the equipment owner?",
+  text: "Once confirmed by owner, you will receive a notification",
+  icon: "success",
+  buttons: true,
+  dangerMode: false,
+}, (willConfirm) => {
+  if (willConfirm) {
+    // swal("Bam, Your booking has been created!", {
+    //   icon: "success",
+    // }).then(() => {
+      const form = document.getElementById('new_booking');
+      form.submit()
+   // });
+  } else {
+    swal("Your booking has been cancelled!");
+  }
+});
