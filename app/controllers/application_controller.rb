ad7833516f8f8203aa7782dabf59ef56698a2078
Robert Simon <robertsimonmedia@gmail.com>
2019-05-23 10:38:48 -0400

    fixed redirect when logging in from show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,22 @@ class ApplicationController < ActionController::Base
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  after_action :store_action
 
-  
+  def store_action
+    return unless request.get?
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      store_location_for(:user, request.fullpath)
+    end
+  end
+
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :avatar_photo, :zipcode])
