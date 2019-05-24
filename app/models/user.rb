60f8c6eb0faa410072aa643b5b5ad322faf38d12
Robert Simon <robertsimonmedia@gmail.com>
2019-05-24 10:28:11 -0400

    made profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   mount_uploader :avatar_photo, PhotoUploader
+  has_many :equipment
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
