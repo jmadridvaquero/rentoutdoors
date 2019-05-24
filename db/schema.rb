27410f99635925db7a97394407980731ca708abf
Simon dHarcourt <simondharcourt@gmail.com>
2019-05-24 13:47:49 -0400

    you cannot edit your equipment now

--- db/schema.rb
+++ db/schema.rb
@@ -51,12 +51,12 @@ ActiveRecord::Schema.define(version: 2019_05_24_141512) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
-    t.string "description"
+    t.text "description"
     t.integer "rating"
-    t.bigint "booking_id"
+    t.bigint "equipment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+    t.index ["equipment_id"], name: "index_reviews_on_equipment_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -78,5 +78,5 @@ ActiveRecord::Schema.define(version: 2019_05_24_141512) do
   add_foreign_key "bookings", "users"
   add_foreign_key "equipment", "users"
   add_foreign_key "equipment_attachments", "equipment"
-  add_foreign_key "reviews", "bookings"
+  add_foreign_key "reviews", "equipment"
 end
